313048f9b885678e6400d7663544dc3a8a64d808
hihassan1998 <hassanihussain1998@gmail.com>
2025-04-19 17:20:08 +0200

    Update index.html, add stylings and update tankyou.php

---
+++ thankyou.php
@@ -0,0 +1,26 @@
+<?php
+// thankyou.php
+?>
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Tack för din registrering</title>
+</head>
+
+<body>
+    <header>
+        Registration Lyckades
+    </header>
+    <main>
+        <section>
+            <h1>Tack för din registrering!</h1>
+            <p>Din information har skickats.</p>
+
+        </section>
+    </main>
+</body>
+
+</html>
